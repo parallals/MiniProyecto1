578c19d133cb676cc06bc669b9e8a02dc8caee89
dievenegas <diegovenegasanabalon@gmail.com>
2023-04-21 23:06:51 -0400

    Correcciones en constructor de ListArray

--- ListArr.cpp
+++ ListArr.cpp
@@ -1,17 +1,18 @@
 #include "ListArr.h"
 
 void ListArr::crearArbol(int iterations, SummaryNode* nodo, int indData, DataNode* T){
-    if(iterations > 1){
-        nodo->left = new SummaryNode(nullptr, nullptr);
-        nodo->right = new SummaryNode(nullptr, nullptr);
+    if(iterations > 2){
+        nodo->left = new SummaryNode(iterations*tam/2);
+        nodo->right = new SummaryNode(iterations*tam/2);
         crearArbol(iterations/2, nodo->left, indData, T);
         crearArbol(iterations/2, nodo->right, indData+(iterations/2), T);
-    } else if(iterations == 1){
+    } else {
         for(int i=0 ; i<indData ; i++){
             T = T->next;
         }
         nodo->dataLeft = T;
         nodo->dataRight = T->next;
+
     }
 }
 
@@ -20,7 +21,7 @@ ListArr::ListArr(){
     for(int i=0 ; i<cantidadNodos ; i++){
         T = new DataNode(tam, 0, new int[tam], T) ;
     }
-    root = new SummaryNode(nullptr, nullptr);
+    root = new SummaryNode(cantidadNodos*tam);
     crearArbol(cantidadNodos, root, 0, T);
 
 }
--- ListArr.h
+++ ListArr.h
@@ -5,24 +5,30 @@ class ListArr{
     private:
         struct DataNode{            // -BORRAR- Nodo con el array.
             int N;                  // -BORRAR- Capacidad maxima del array
+            int n;                  // -BORRAR- Cantidad usada.
             int* array;             // -BORRAR- Array dinamico.
-            int n;              // -BORRAR- Cantidad usada.
             DataNode* next;
             DataNode(int N, int n, int* array, DataNode* next){
                 this->N = N;
                 this->array = array;
                 this->n = n;
-                this-> next = next;
+                this->next = next;
             }
         };
         struct SummaryNode{         // -BORRAR- Nodo de direcciones izquierda y derecha.
+            int N;                  // -BORRAR- Capacidad maxima.
+            int n;                  // -BORRAR- Cantidad usada.
             SummaryNode* right;     // -BORRAR- Puntero a Nodo de izquierda, que contiene a los otros nodos.
             SummaryNode* left;      // -BORRAR- Puntero a Nodo de Derecha, que contiene a los otros nodos.
             DataNode* dataLeft;            // -BORRAR- Puntero a Nodo con array de Izquierda.
             DataNode* dataRight;            // -BORRAR- Puntero a Nodo con array de Derecha.
-            SummaryNode(DataNode* dataRight, DataNode* dataLeft){ // -BORRAR- En caso de no haber algun node se asigna Nullptr.
-                this->dataRight = dataRight;
-                this->dataLeft = dataLeft;
+            SummaryNode(int N){ // -BORRAR- En caso de no haber algun node se asigna Nullptr.
+                this->N = N;
+                n = 0;
+                right = nullptr;
+                left = nullptr;
+                dataLeft = nullptr;
+                dataRight = nullptr;
             }            
         };
         int cantidadNodos = 8; // numero de nodos de datos.
